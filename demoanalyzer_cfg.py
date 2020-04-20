336da5aaad62bce272e6389a323cc695bcf7712a
FQMeng <mengfanqiang@pku.edu.cn>
2020-04-20 16:05:38 +0800

    change file structure

--- demoanalyzer_cfg.py
+++ demoanalyzer_cfg.py
@@ -8,24 +8,22 @@ process = cms.Process("Demo")
 process.load("FWCore.MessageLogger.MessageLogger_cfi")
 process.MessageLogger.cerr.threshold = 'INFO'
 process.MessageLogger.categories.append('Demo')
-process.MessageLogger.cerr.INFO = cms.untracked.PSet(
-        limit = cms.untracked.int32(-1)
-        )
-process.options = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
+process.MessageLogger.cerr.INFO = cms.untracked.PSet(limit=cms.untracked.int32(-1))
+process.options = cms.untracked.PSet(wantSummary=cms.untracked.bool(True))
 # **********************************************************************
 # set the maximum number of events to be processed                     *
 #    this number (argument of int32) is to be modified by the user     *
 #    according to need and wish                                        *
 #    default is preset to 10000 events                                 *
 # **********************************************************************
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(10000) )
+process.maxEvents = cms.untracked.PSet(input=cms.untracked.int32(10000))
 
 # set the number of events to be skipped (if any) at end of file below
 
 # define JSON file for 2011 (replace by 2012 version for 2012 data)
 goodJSON = 'datasets/Cert_160404-180252_7TeV_ReRecoNov08_Collisions11_JSON.txt'
 
-myLumis = LumiList.LumiList(filename = goodJSON).getCMSSWString().split(',')
+myLumis = LumiList.LumiList(filename=goodJSON).getCMSSWString().split(',')
 
 # ****************************************************************************
 # define the input data set here by inserting the appropriate .txt file list *
@@ -43,11 +41,15 @@ import FWCore.Utilities.FileUtils as FileUtils
 # *** SingleMu data set ***
 # files2011data = FileUtils.loadListFromFile ('datasets/CMS_Run2011A_SingleMu_AOD_12Oct2013-v1_10000_file_index.txt')
 # *** DoubleMu data set ***
-files2011data = FileUtils.loadListFromFile ('datasets/CMS_Run2011A_DoubleMu_AOD_12Oct2013-v1_10000_file_index.txt')
-process.source = cms.Source("PoolSource",
-    fileNames = cms.untracked.vstring(*files2011data
-    )
-)
+# files2011data = FileUtils.loadListFromFile('datasets/CMS_Run2011A_DoubleMu_AOD_12Oct2013-v1_10000_file_index.txt')
+# *** DoubleMu data sets, full ***
+files2011data = []
+datasets = FileUtils.os.walk(r"./datasets/Double")
+for path, dir_list, file_list in datasets:
+    for indexfile in file_list:
+        files2011data.extend(FileUtils.loadListFromFile(FileUtils.os.path.join(path, indexfile)))
+
+process.source = cms.Source("PoolSource", fileNames=cms.untracked.vstring(*files2011data))
 
 # apply JSON file
 #   (needs to be placed *after* the process.source input file definition!)
@@ -59,17 +61,15 @@ process.source.lumisToProcess.extend(myLumis)
 # *************************************************
 process.source.skipEvents = cms.untracked.uint32(0)
 
-process.demo = cms.EDAnalyzer('DimuonSpectrum2011'
-)
+process.demo = cms.EDAnalyzer('DimuonSpectrum2011')
 # ***********************************************************
 # output file name                                          *
 # default is DoubleMu.root                                        *
 # change this according to your wish                        *
 # ***********************************************************
-process.TFileService = cms.Service("TFileService",
-#       fileName = cms.string('SingleMu.root')
-       fileName = cms.string('DoubleMu.root')
-                                   )
-
+process.TFileService = cms.Service(
+    "TFileService",
+    #       fileName = cms.string('SingleMu.root')
+    fileName=cms.string('DoubleMu2011.root'))
 
 process.p = cms.Path(process.demo)
